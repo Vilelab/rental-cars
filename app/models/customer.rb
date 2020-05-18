6ba8f611ce372ab345d1c26f7c42121a45c72cc8
Bruno Vilela <bruno.boccia@fatec.sp.gov.br>
2020-05-18 13:44:03 -0300

    Usuario procura cliente pelo nome parcial ou documento

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -1,4 +1,10 @@
 class Customer < ApplicationRecord
   has_many :rentals
   has_many :car_categories
+
+  def self.search(query) #self diz que trata-se de um método de classe...
+                         #é um met. de classe porque vamos usá-lo em todas as instâncias de customer...
+    scope :search, ->(query) { where('name LIKE ?', "%#{query}%")
+                               .or(where(document: query)) }
+  end  
 end
